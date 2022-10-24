68dc16b6cf83f8a6d3fc2232ecd609ab70bb7c7f
Fabian Dietz <fabian.dietz@tmt.de>
2022-10-24 15:25:00 +0200

    Added documentation

--- broadcastchannel.go
+++ broadcastchannel.go
@@ -7,25 +7,28 @@ import (
 	"time"
 )
 
-type Channel[T any] struct {
+// Broadcastchannel is a pub/sub channel. All input is replicated to all outputs.
+type Broadcastchannel[T any] struct {
 	input          chan T
-	output         []*OutputChannel[T]
+	output         []*outputChannel[T]
 	outputLock     sync.RWMutex
 	context        context.Context
 	logger         Logger
 	receiveTimeout *time.Duration
 }
 
-type OutputChannel[T any] struct {
+type outputChannel[T any] struct {
 	id      uuid.UUID
 	channel chan T
 }
 
-func NewChannel[T any](ctx context.Context, logger Logger) *Channel[T] {
-	var channel Channel[T]
+// NewChannel creates a new broadcast channel, given ctx as Context for cancellation.
+// logger may be nil
+func NewChannel[T any](ctx context.Context, logger Logger) *Broadcastchannel[T] {
+	var channel Broadcastchannel[T]
 	//Blocking channel
 	channel.input = make(chan T, 0)
-	channel.output = make([]*OutputChannel[T], 0)
+	channel.output = make([]*outputChannel[T], 0)
 	channel.context = ctx
 	if logger != nil {
 		channel.logger = logger
@@ -34,7 +37,14 @@ func NewChannel[T any](ctx context.Context, logger Logger) *Channel[T] {
 	return &channel
 }
 
-func (ch *Channel[T]) Publish(val T) error {
+// WithReceiveChannelTimeout may be chained to have a seperate timeout for each message on each receipient.
+func (ch *Broadcastchannel[T]) WithReceiveChannelTimeout(timeout time.Duration) *Broadcastchannel[T] {
+	ch.receiveTimeout = &timeout
+	return ch
+}
+
+// Publish publishes given value val to all subscribers
+func (ch *Broadcastchannel[T]) Publish(val T) error {
 	err := ch.context.Err()
 	if err != nil {
 		return err
@@ -46,19 +56,15 @@ func (ch *Channel[T]) Publish(val T) error {
 	return nil
 }
 
-func (ch *Channel[T]) WithReceiveChannelTimeout(timeout time.Duration) *Channel[T] {
-	ch.receiveTimeout = &timeout
-	return ch
-}
-
-func (ch *Channel[T]) Subscribe(subscriber chan T) (uuid.UUID, error) {
+// Subscribe subscribes a channel to the broadcast channel. An uuid ist returned to be able to unsubscribe from the broadcast channel und for debug log naming.
+func (ch *Broadcastchannel[T]) Subscribe(subscriber chan T) (uuid.UUID, error) {
 	err := ch.context.Err()
 	if err != nil {
 		return uuid.Nil, err
 	}
 	ch.outputLock.Lock()
 	uid := uuid.New()
-	ch.output = append(ch.output, &OutputChannel[T]{channel: subscriber, id: uid})
+	ch.output = append(ch.output, &outputChannel[T]{channel: subscriber, id: uid})
 	if ch.logger != nil {
 		ch.logger.Debugf("New channel subscriber: %s", uid)
 	}
@@ -66,12 +72,13 @@ func (ch *Channel[T]) Subscribe(subscriber chan T) (uuid.UUID, error) {
 	return uid, nil
 }
 
-func (ch *Channel[T]) Unsubscribe(subscriptionId uuid.UUID) {
+// Unsubscribe unsubscribes a channel from the broadcast channel. It will not receive further broadcasts. It will not close the channel.
+func (ch *Broadcastchannel[T]) Unsubscribe(subscriptionId uuid.UUID) {
 	if ch.logger != nil {
 		ch.logger.Debugf("Remove channel subscriber: %s", subscriptionId.String())
 	}
 	ch.outputLock.Lock()
-	newOutput := make([]*OutputChannel[T], 0, len(ch.output)-1)
+	newOutput := make([]*outputChannel[T], 0, len(ch.output)-1)
 	for _, sub := range ch.output {
 		if sub.id != subscriptionId {
 			newOutput = append(newOutput, sub)
@@ -81,7 +88,7 @@ func (ch *Channel[T]) Unsubscribe(subscriptionId uuid.UUID) {
 	ch.outputLock.Unlock()
 }
 
-func (ch *Channel[T]) broadcast() {
+func (ch *Broadcastchannel[T]) broadcast() {
 	for {
 		select {
 		case val := <-ch.input:
@@ -104,11 +111,13 @@ func (ch *Channel[T]) broadcast() {
 	}
 }
 
-func (ch *Channel[T]) send(channel *OutputChannel[T], value T) {
+func (ch *Broadcastchannel[T]) send(channel *outputChannel[T], value T) {
 	var ctx context.Context
+	var indivCancelFunc func()
 	if ch.receiveTimeout != nil {
-		ictx, _ := context.WithTimeout(ch.context, *ch.receiveTimeout)
+		ictx, cancelFunc := context.WithTimeout(ch.context, *ch.receiveTimeout)
 		ctx = ictx
+		indivCancelFunc = cancelFunc
 	} else {
 		ctx = ch.context
 	}
@@ -117,6 +126,9 @@ func (ch *Channel[T]) send(channel *OutputChannel[T], value T) {
 	case <-ctx.Done():
 		if ch.logger != nil {
 			ch.logger.Debugf("Could not deliver message to receiver %s: %s, channel blocking?", channel.id.String(), ctx.Err())
+		}
+		if indivCancelFunc != nil {
+			indivCancelFunc()
 		}
 	}
 }
